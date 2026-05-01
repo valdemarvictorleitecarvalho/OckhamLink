eb0980ca424e7ddd6b8b7ec54eab99db58362208
valdemarvictorleitecarvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2026-05-01 12:59:49 -0300

    feat(frontend): setup vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    passWithNoTests: true, 
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['src/**/*.spec.{ts,tsx}', 'src/main.tsx'],
+    },
+  },
+});
