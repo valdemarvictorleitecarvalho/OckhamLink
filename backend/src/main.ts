cf22082c2a15b6d4259e37a4069584b5b13d65f4
valdemarvictorleitecarvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2026-05-03 16:46:31 -0300

    fix(main): expose nestjs port to 0.0.0.0 for cloudrun

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   setupValidation(app);
   setupSwagger(app);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
